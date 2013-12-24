ddc000a0eebc9c705d1211559d25c97b0263951a
ibrain <kalips0mr@gmail.com>
2013-12-24 16:44:16 +0300

    Fixed unique checking
    Affected like count and visit count mechanism

--- metrics.class.php
+++ metrics.class.php
@@ -6,11 +6,15 @@
 		function SetVisitor($metrics_id)
 		{
 
-				$NoSQLMetricsData = file_get_contents("store.json");
-				$NoSQLMetricsData = json_decode($NoSQLMetricsData,true);
-				$NoSQLMetricsData["pages"][$metrics_id]["visits"] = intval($NoSQLMetricsData["pages"][$metrics_id]["visits"])+1;
-				$NoSQLMetricsData = json_encode($NoSQLMetricsData);
-				file_put_contents("store.json",$NoSQLMetricsData);
+			if(!isset($_COOKIE["visit_$metrics_id"]))
+			{
+					$NoSQLMetricsData = file_get_contents("store.json");
+					$NoSQLMetricsData = json_decode($NoSQLMetricsData,true);
+					$NoSQLMetricsData["pages"][$metrics_id]["visits"] = intval($NoSQLMetricsData["pages"][$metrics_id]["visits"])+1;
+					$NoSQLMetricsData = json_encode($NoSQLMetricsData);
+					file_put_contents("store.json",$NoSQLMetricsData);
+					SetCookie("visit_$metrics_id",'VISIT_SET',time()+86400);
+			}
 
 		}
 		
@@ -20,14 +24,14 @@
 		function SetLike($metrics_id)
 		{
 		
-		if(!isset($_COOKIE['like']))
+		if(!isset($_COOKIE["like_$metrics_id"]))
 			{
 			$NoSQLMetricsData = file_get_contents("store.json");
 			$NoSQLMetricsData = json_decode($NoSQLMetricsData,true);
 			$NoSQLMetricsData["pages"][$metrics_id]["likes"] = intval($NoSQLMetricsData["pages"][$metrics_id]["likes"])+1;
 			$NoSQLMetricsData = json_encode($NoSQLMetricsData);
 			file_put_contents("store.json",$NoSQLMetricsData);
-			SetCookie("like",'LIKE_SET',time()+86400);
+			SetCookie("like_$metrics_id",'LIKE_SET',time()+86400);
 			}
 		}
 		
